7cfe409b33b6aa8c937722271d1004075b21aad5
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-17 22:01:50 -0400

    Agregando la configuracion general de la app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), 
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      database: process.env.DATABASE_NAME,
+      username: process.env.USERNAME_DB,
+      password: process.env.PASSWORD_DB,  
+      retryDelay: 3000,    
+      autoLoadEntities: true,
+      synchronize: false, //Ya la estructura SQL esta hecha, solo usaremos el orm para interacciones
+    }), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/dashboard');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
+
+  await app.listen(3002);
 }
 bootstrap();
