c98299207dde14413009f4d569e8926de69fd403
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-19 09:12:04 -0400

    Agregando los modulos de mailing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { MailingModule } from './mailing/mailing.module';
 
 @Module({
   imports: [
@@ -19,7 +21,12 @@ import { DashboardModule } from './dashboard/dashboard.module';
       retryDelay: 3000,    
       autoLoadEntities: true,
       synchronize: false, //Ya la estructura SQL esta hecha, solo usaremos el orm para interacciones
-    }), DashboardModule, 
+    }), 
+    DashboardModule, 
+    MailingModule, 
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
